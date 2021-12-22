8fcdc0dfedc6149701f539ee4f0669c245c8694b
ValerioRegini <6963119@gmail.com>
2021-12-22 18:56:58 +0300

    Простое число [#171694 #242830]

--- src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -2,7 +2,6 @@ package ru.job4j.loop;
 
 public class CheckPrimeNumber {
     public static boolean check(int number) {
-        boolean result = true;
         for (int index = 2; index < number; index++) {
             if (number % index == 0) {
                 return false;
