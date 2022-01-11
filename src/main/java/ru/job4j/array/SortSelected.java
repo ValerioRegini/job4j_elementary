80629f5094b1862617b0decedfd961858fb9c285
ValerioRegini <6963119@gmail.com>
2022-01-11 20:30:10 +0300

    Сортировка выборкой. [#156320]

---
+++ src/main/java/ru/job4j/array/SortSelected.java
@@ -0,0 +1,17 @@
+package ru.job4j.array;
+
+public class SortSelected {
+    public static int[] sort(int[] data) {
+        for (int i = 0; i < data.length; i++) {
+            int min = MinDiapason.findMin(data, 0, data.length - 1);
+            int index = FindLoop.indexOf(data, min, 0, data.length - 1);
+            /* swap(...) */
+            if (i != index) {
+                int temp = data[index];
+                data[index] = min;
+                data[min] = temp;
+            }
+        }
+        return data;
+    }
+}
