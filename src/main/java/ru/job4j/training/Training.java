f2695e1c723a1f0dfccce9ceb227b50c9cce7003
ValerioRegini <6963119@gmail.com>
2022-02-17 10:25:31 +0300

    test

---
+++ src/main/java/ru/job4j/training/Training.java
@@ -0,0 +1,9 @@
+package ru.job4j.training;
+
+class Training {
+        public static void out(int m, int n) {
+            for (int i = n - m; i > n; i++) {
+                System.out.println(n);
+            }
+        }
+    }
